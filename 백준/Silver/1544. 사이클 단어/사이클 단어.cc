1bb0a7d82ab4436b326d93c78a973c6746b1f3b8
besyia0k0 <besyia0k0@cau.ac.kr>
2022-03-25 16:22:21 +0900

    [Silver IV] Title: 사이클 단어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1544. 사이클 단어/사이클 단어.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string>	word;
+
+void	fastio(void);
+void	find_string(string s);
+
+int main(void)
+{
+    int	num;
+	string	tmp;
+	
+	fastio();
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		cin >> tmp;
+		find_string(tmp);
+	}
+	cout << word.size() << "\n";
+	return (0);
+}
+
+void	find_string(string s)
+{
+	int	vec_size = word.size();
+	int	str_size = s.length();
+
+	for (int i = 0; i < vec_size; i++)
+	{
+		if (word[i].length() == str_size)
+		{
+			size_t idx = s.find(word[i][0]);
+			while (idx != string::npos)
+			{
+				if (s.substr(idx) == word[i].substr(0, str_size - idx))
+				{
+					if (s.substr(0, idx) == word[i].substr(str_size - idx))
+						return ;
+				}
+				idx = s.find(word[i][0], idx + 1);
+			}
+		}
+	}
+	word.push_back(s);
+}
+
+void	fastio(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
