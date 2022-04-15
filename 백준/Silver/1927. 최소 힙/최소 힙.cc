7f18d83d7d689d9f3833e5205f962c5de424b776
Hyeonsol Kong <besyia0k0@cau.ac.kr>
2022-04-16 01:29:56 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#define fastio                      \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL)
+using namespace std;
+
+int main(void)
+{
+	int n, tmp;
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	fastio;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		if (tmp == 0)
+		{
+			if (pq.empty())
+				cout << "0\n";
+			else
+			{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+			pq.push(tmp);
+	}
+	return (0);
+}
