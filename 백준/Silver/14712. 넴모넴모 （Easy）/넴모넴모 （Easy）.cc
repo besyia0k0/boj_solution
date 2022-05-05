9cb3a3fc742dadf9c7c2a701f0a6a3b65d34f73f
Hyeonsol Kong <besyia0k0@cau.ac.kr>
2022-05-05 16:57:49 +0900

    [Silver I] Title: 넴모넴모 (Easy), Time: 364 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#define fastio                      \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL)
+using namespace std;
+
+bool visited[26][26];
+bool map[26][26];
+int result, n, m;
+void recur(int times);
+
+int main(void)
+{
+	cin >> n >> m;
+	recur(0);
+	cout << result << "\n";
+	return (0);
+}
+
+void recur(int times)
+{
+	int x = times / m, y = times % m;
+
+	if (times == n * m)
+	{
+		result++;
+		return;
+	}
+	if (!visited[x][y])
+	{
+		if (!(x > 0 && y > 0 && (map[x - 1][y] && map[x][y - 1] && map[x - 1][y - 1])))
+		{
+			visited[x][y] = true;
+			map[x][y] = true;
+			recur(x * m + y + 1);
+			visited[x][y] = false;
+			map[x][y] = false;
+		}
+		visited[x][y] = true;
+		map[x][y] = false;
+		recur(x * m + y + 1);
+		visited[x][y] = false;
+		map[x][y] = false;
+	}
+}
